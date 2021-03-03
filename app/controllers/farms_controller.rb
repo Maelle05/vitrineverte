82588ce31812919ad7a408b002e641d1a460015b
valentingnt <valentin64.genest@gmail.com>
2021-03-03 11:39:57 +0100

    changed conditions --> more logical

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,5 +1,5 @@
 class FarmsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   before_action :set_farm, only: [:show, :edit, :update, :destroy]
 
   # add_breadcrumb "home", :root_path
